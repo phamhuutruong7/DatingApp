141a4be11b7ed54fa23069b98f7859d670bfda1d
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2022-01-13 04:17:48 +0100

    Using formBuilder service

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { ToastrService } from 'ngx-toastr';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 
 @Component({
@@ -15,23 +15,24 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
   constructor(private accountService: AccountService,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initializeForm();
   }
 
   initializeForm(){
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', [
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [
         Validators.required, 
         Validators.minLength(4),
         Validators.maxLength(8)
-      ]),
-      confirmPassword: new FormControl('',[
+      ]],
+      confirmPassword: ['',[
         Validators.required, this.matchValues('password')
-      ])
+      ]]
     })
     this.registerForm.controls.password.valueChanges.subscribe(() => {
       this.registerForm.controls.confirmPassword.updateValueAndValidity();
