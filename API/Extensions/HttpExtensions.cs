d0077e52a2d298fee663437e56bfce45f7dd8fd3
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2022-01-14 23:58:50 +0100

    Adding helper classes for pagination

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,18 @@
+﻿using API.Helpers;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response,int currentPage,int itemsPerPage,
+            int totalItems,int totalPages
+            )
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
