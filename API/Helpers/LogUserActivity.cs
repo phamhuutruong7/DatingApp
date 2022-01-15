17e2e448ab6434fa08a8457b1126d8a844f1d18e
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2022-01-15 05:22:21 +0100

    Add action filter

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,22 @@
+﻿using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+            if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return; //not authenticated
+            var username = resultContext.HttpContext.User.GetUsername();
+            var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
+            var user = await repo.GetUserByUsernameAsync(username);
+            user.LastActive = DateTime.Now();
+            await repo.SaveAllAsync();
+        }
+    }
+}
