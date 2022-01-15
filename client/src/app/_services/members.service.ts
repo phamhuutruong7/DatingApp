371d130e21f5dff7a30f170d6f56f8bdc827c7c0
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2022-01-15 06:29:27 +0100

    Restoring cache for members

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -13,10 +13,15 @@ import { UserParams } from '../_models/userParams';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
 
   constructor(private http: HttpClient) { }
   
   getMembers(userParams: UserParams){
+    var response = this.memberCache.get(Object.values(userParams).join('-'));
+    if(response){
+      return of (response);
+    }
 
     let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
     
@@ -25,7 +30,11 @@ export class MembersService {
     params = params.append('gender', userParams.gender);
     params = params.append('order', userParams.orderBy);
     
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params);
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params)
+      .pipe(map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      }))
   }
 
   private getPaginatedResult<T>(url, params){
