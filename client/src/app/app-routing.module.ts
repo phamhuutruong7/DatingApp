0a6565dc3c7f6167fd56476f8a56da223070580d
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2022-01-26 01:38:51 +0100

    Using route resolvers (of Angular)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
@@ -21,7 +22,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children:[
       { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-      { path: 'members/:username', component: MemberDetailComponent},
+      { path: 'members/:username', component: MemberDetailComponent, resolve:{member: MemberDetailedResolver}},
       { path: 'member/edit', component: MemberEditComponent, canDeactivate:[PreventUnsavedChangesGuard]},
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -25,7 +25,9 @@ export class MemberDetailComponent implements OnInit {
     private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe(data => {
+      this.member = data.member;
+    })
     this.route.queryParams.subscribe(params =>{
       params.tab ? this.selectTab(params.tab) : this.selectTab(0)
     })
@@ -39,6 +41,7 @@ export class MemberDetailComponent implements OnInit {
         preview: false
       }
     ]
+    this.galleryImages = this.getImages();
   }
 
   getImages(): NgxGalleryImage[] {
@@ -53,12 +56,6 @@ export class MemberDetailComponent implements OnInit {
     return imageUrls;
   }
 
-  loadMember(){
-    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member => {
-      this.member = member;
-      this.galleryImages = this.getImages();
-    })
-  }
 
   loadMessages(){
     this.messageService.getMessageThread(this.member.username).subscribe(messages =>{
