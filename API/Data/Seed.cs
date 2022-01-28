e3f4024d634168d1fdbb08f0e06b91b1f5e11362
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2022-01-28 21:48:50 +0100

    Setting up the entities. Entities should implement from Identity of Microsoft. 1 User will have many roles. 1 role has many user.

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -17,10 +17,8 @@ namespace API.Data
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             foreach(var user in users)
             {
-                using var hmac = new HMACSHA512();
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("password"));
-
+                
                 await context.Users.AddAsync(user);
             }
             await context.SaveChangesAsync();
