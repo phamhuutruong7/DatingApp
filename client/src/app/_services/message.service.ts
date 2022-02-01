e2e0e63b0f50286e59294c4b0aad0cd65a222447
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2022-02-01 22:50:26 +0100

    Optimizing the messages

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,3 +1,4 @@
+import { Group } from './../_models/group';
 import { BehaviorSubject } from 'rxjs';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { environment } from './../../environments/environment';
@@ -38,6 +39,19 @@ export class MessageService {
       this.messageThread$.pipe(take(1)).subscribe(messages => {
         this.messageThreadSource.next([...messages, message])
       })
+    })
+
+    this.hubConnection.on('UpdatedGroup', (group: Group) => {
+      if(group.connections.some(x => x.username === otherUsername)){
+        this.messageThread$.pipe(take(1)).subscribe(messages => {
+          messages.forEach(message => {
+            if(!message.dateRead){
+              message.dateRead = new Date(Date.now())
+            }
+          })
+          this.messageThreadSource.next([...messages]);
+        })
+      }
     })
 
   }
