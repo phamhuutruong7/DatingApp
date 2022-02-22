e441d870c0a81624dcd53da3b681720b3b9c9b48
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2022-02-22 23:09:00 +0100

    Tidying up the member message component

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,10 +1,11 @@
 import { MembersService } from 'src/app/_services/members.service';
 import { MessageService } from './../../_services/message.service';
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Message } from 'src/app/_models/message';
 import { NgForm } from '@angular/forms';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css']
